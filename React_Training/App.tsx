7b2e04ef6afe85996bf49bed7ef9450a90da2b7b
Rajdeep Rao <raorajdeep30@gmail.com>
2023-02-07 08:32:24 +0530

    IMDb MOvies_Rating

---
+++ React_Training/App.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { View, Image, Text, FlatList,TouchableOpacity,StyleSheet } from 'react-native';
+
+const IMDbMovieList = () => {
+  const movies = [
+    {
+      id: '1',
+      title: 'The Shawshank Redemption',
+      image: require('./App/Assets/Images/theShRed.jpg'),
+      rating: 9.2
+    },
+    {
+      id: '2',
+      title: 'Inception',
+      image: require('./App/Assets/Images/inception@.jpeg'),
+      rating: 9.2
+    },
+    {
+      id: '3',
+      title: 'interstellar',
+      image: require('./App/Assets/Images/interstellar.jpg'),
+      rating: 9.0
+    },
+    {
+      id: '4',
+      title: ' Dark',
+      image: require('./App/Assets/Images/dark.jpeg'),
+      rating: 8.8
+    },
+    {
+      id: '5',
+      title: 'The Godfather',
+      image: require('./App/Assets/Images/godfather.jpeg'),
+      rating: 8.7
+    },
+    {
+      id: '6',
+      title: 'The Forest Gump',
+      image: require('./App/Assets/Images/forestgump.jpeg'),
+      rating: 8.6
+    },
+    {
+      id: '7',
+      title: 'The Endgame',
+      image: require('./App/Assets/Images/endgame.jpeg'),
+      rating: 8.4
+    },
+    {
+      id: '8',
+      title: 'Avatar',
+      image: require('./App/Assets/Images/avatar.jpeg'),
+      rating: 8.0
+    },
+
+
+  ];
+
+  const navBar = () => {
+    const left = require('./App/Assets/Images/Hamburger.png')
+    const right = require('./App/Assets/Images/backArrow.png')
+    return (
+      <>
+      <View style={{ backgroundColor: 'red', flexDirection: 'row',paddingVertical:10,alignItems:'center',paddingBottom:20}}>
+       <TouchableOpacity>
+        <Image source={left} style={styles.navIcon}></Image>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={right} style={{...styles.navIcon,marginLeft:270}}></Image>
+          </TouchableOpacity>
+          </View>
+          </>
+
+    )
+  }
+
+  const renderMovie = ({ item }) => (
+    <View style={{ alignItems: 'center' }}>
+      <Image 
+        source={ item.image }
+        style={{ width: 150, height: 220, margin: 10 }}
+      />
+      <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{item.title}</Text>
+      <Text style={{ color: 'grey', fontSize: 14 }}>IMDb rating: {item.rating}</Text>
+    </View>
+  );
+
+  return (
+    <View>
+    {navBar()}
+    <FlatList 
+      numColumns={2}
+      data={movies}
+      renderItem={renderMovie}
+      keyExtractor={item => item.id}
+    />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    width: '100%',
+    marginTop: 10,
+   // backgroundColor: 'yellow'
+  },
+  navIcon: {
+    height: 14,
+    marginHorizontal: 20,
+    marginTop: 10
+  },
+
+})
+export default IMDbMovieList;
+//
